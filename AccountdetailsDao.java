0a8f3faec1c7876773bec58e6ded9e30f896cc58
Ankit Deixit <erankitdixit042@gmail.com>
2024-05-11 15:32:11 +0530

    add account files

---
+++ AccountdetailsDao.java
@@ -0,0 +1,30 @@
+package com.paymentservices.accountdetails;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paymentservices.accountdetails.repository.Accountrepository;
+
+@Service
+public class AccountdetailsDao {
+	
+	@Autowired
+	Accountrepository repository;
+	
+	 List<Accountdetails> accountdetail=new ArrayList();
+	 
+		public List<Accountdetails> findall(){
+			return repository.findAll();
+		}
+	public List<Accountdetails> findone(double balance) {
+		Predicate< Accountdetails> predicate=details->details.getAccountno().equals(accountdetail);
+	 
+		return accountdetail.stream().filter(predicate).get();
+		
+}
+}
