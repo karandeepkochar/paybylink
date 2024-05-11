05fdcac8f7afdf61122d9e9f75b79c1946a02178
karandeepkochar <karandeep.kochar98@gmail.com>
2024-05-11 13:28:24 +0530

    Initial commit

---
+++ src/main/java/com/hcltech/paybylink/PaybylinkApplication.java
@@ -0,0 +1,13 @@
+package com.hcltech.paybylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaybylinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaybylinkApplication.class, args);
+    }
+
+}
