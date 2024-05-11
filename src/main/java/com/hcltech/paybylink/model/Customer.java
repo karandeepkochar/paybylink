570ba1f0dabf80661c8d46b3e05869d6ad91c007
Ashish Amar <ashishamar1999@gmail.com>
2024-05-11 15:38:02 +0530

    add customer dao, repo and service

---
+++ src/main/java/com/hcltech/paybylink/model/Customer.java
@@ -0,0 +1,34 @@
+package com.hcltech.paybylink.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String customerId;
+
+    String username;
+
+    String password;
+
+    @OneToMany(mappedBy = "customer")
+    List<Account> accounts;
+
+    String customerName;
+
+    String email;
+
+    String phNo;
+
+
+}
